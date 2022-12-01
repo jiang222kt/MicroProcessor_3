6fce66c53f3c2ff7130939cfcbb31309effde8ba
jiang222kt <jiang222kt@gmail.com>
2022-12-01 15:51:59 +0900

    update alu

--- cpu/alu.v
+++ cpu/alu.v
@@ -9,15 +9,17 @@ module alu(
 );
 
 always @(*) begin
-    br_taken = `DISABLE;
     if(alucode == `ALU_ADD || alucode == `ALU_LB || alucode == `ALU_LH || alucode == `ALU_LW || alucode == `ALU_LBU || alucode == `ALU_LHU || alucode == `ALU_SB || alucode == `ALU_SH) begin
         alu_result <= op1 + op2;
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_SUB) begin
         alu_result <= op1 - op2;
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_LUI) begin 
         alu_result <= 32'b0;
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_JAL) begin
         alu_result <= op2 + 4;
@@ -29,48 +31,62 @@ always @(*) begin
     end
     else if(alucode == `ALU_SLT) begin
         alu_result <= (($signed(op1) < $signed(op2))? {{31'b0}, {1'b1}}: 32'b0);
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_SLTU) begin
         alu_result <= (($unsigned(op1) < $unsigned(op2))? {{31'b0}, {1'b1}}: 32'b0);
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_XOR) begin
         alu_result <= op1 ^ op2;
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_OR) begin
         alu_result <= op1 | op2;
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_AND) begin
         alu_result <= op1 & op2;
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_SLL) begin
         alu_result <= (op1 << op2[4:0]);
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_SRL) begin
         alu_result <= (op1 >> op2[4:0]);
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_SRA) begin
         alu_result <= ($signed(op1) >>> op2[4:0]);
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_BEQ) begin
         alu_result <= 32'b0;
     end
     else if(alucode == `ALU_BNE) begin
         alu_result <= 32'b0;
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_BLT) begin
         alu_result <= 32'b0;
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_BGE) begin
         alu_result <= 32'b0;
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_BLTU) begin
         alu_result <= 32'b0;
+        br_taken <= `DISABLE;
     end
     else if(alucode == `ALU_BGEU) begin
         alu_result <= 32'b0;
+        br_taken <= `DISABLE;
     end
     else begin
         alu_result = 32'b0;
+        br_taken <= `DISABLE;
     end
     end
 endmodule
