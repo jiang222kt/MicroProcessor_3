7c4ff92fb9854b9ccdda6620bd059dff6330e9c9
jiang222kt <jiang222kt@gmail.com>
2022-12-05 13:27:46 +0900

    temporary saved decoder

--- cpu/decoder.v
+++ cpu/decoder.v
@@ -12,18 +12,17 @@ module decoder(
     output reg	     	reg_we,       // レジスタ書き込みの有無
     output reg		is_load,      // ロード命令判定フラグ
     output reg		is_store,     // ストア命令判定フラグ
-    output reg          is_halt
+    output reg      is_halt
 );
 
 wire [6:0] op = ir[6:0];
 wire [6:0] funct7 = ir[31:25];
 wire [2:0] funct3 = ir[14:12];
-reg [31:0] reg_imm;
-assign imm = reg_imm;
 
 assign srcreg1_num = (op == `LUI || op == `AUIPC || op == `JAL)?  5'b0 : ir[19:15];
 assign srcreg2_num = (op == `OP || op == `JALR || op == `STORE)? ir[24:20] : 5'b0;
 assign dstreg_num = (op == `BRANCH || op == `STORE)? 5'b0 : ir[11:7];
+assign imm = (op == `)
 
 
 always @(*) begin
