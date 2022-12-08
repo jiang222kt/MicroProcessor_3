25c4dcca9fbcdb4bd62cf2cced1f000953325af4
jiang222kt <jiang222kt@gmail.com>
2022-12-08 13:58:18 +0900

    make data, instruction, register

---
+++ cpu/data.v
@@ -0,0 +1,23 @@
+module data_memory(
+  input clk,
+  input [31:0] addr,
+  input [31:0] data_in,
+  output reg [31:0] data_out,
+  input write_enable
+);
+
+reg [31:0] mem [0:100000];
+
+// データを書き込む処理
+always @ (posedge clk) begin
+  if (write_enable) begin
+    mem[addr] <= data_in;
+  end
+end
+
+// データを読み出す処理
+always @ (posedge clk) begin
+  data_out <= mem[addr];
+end
+
+endmodule
--- cpu/instruction.v
+++ cpu/instruction.v
@@ -1,4 +1,5 @@
-module instruction(
+module instruction_memory(
+    input clk,
     input wire [31:0] PC,
     output reg [31:0] instruction
 );
@@ -6,7 +7,7 @@ module instruction(
 reg [31:0] mem [0:100000];
 initial $readmemh("/home/denjo/b3exp/benchmarks/tests/LoadAndStore/code.hex", mem);
 
-always @(*) begin
+always @(posedge clk) begin
     instruction <= mem[PC];
 end
 
